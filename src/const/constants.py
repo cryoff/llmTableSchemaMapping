7370a56b07725b115250591b795804f65f1f8443
cryoff <cardigan99@mail.ru>
2023-09-24 18:54:10 +0200

    [LLM-4] Add constants separately

---
+++ src/const/constants.py
@@ -0,0 +1,7 @@
+from numpy.random import RandomState
+
+RANDOM_STATE: RandomState = RandomState(42)
+SAMPLE_SIZE: int = 50
+DATE_CATEGORY: str = "date"
+NUMBER_CATEGORY: str = "number"
+TEXT_CATEGORY: str = "text"
